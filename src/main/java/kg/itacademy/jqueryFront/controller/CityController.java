279d1bb8711d2b866f8eb5a899135b48de361a99
Askhat <serikov.askhat@gmail.com>
2020-05-16 22:10:47 +0600

    images

--- src/main/java/kg/itacademy/jqueryFront/controller/CityController.java
+++ src/main/java/kg/itacademy/jqueryFront/controller/CityController.java
@@ -4,7 +4,12 @@ import kg.itacademy.jqueryFront.entity.City;
 import kg.itacademy.jqueryFront.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -28,5 +33,19 @@ public class CityController {
     @GetMapping("/{id}")
     public City getById(@PathVariable("id") Long id) {
         return cityService.getById(id);
+    }
+
+    @PostMapping("/image")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String fileName = addImage(file);
+        return fileName;
+    }
+
+    private String addImage(MultipartFile file) throws IOException {
+        byte[] bytes = file.getBytes();
+        String modifiedFileName = System.currentTimeMillis() + file.getOriginalFilename();
+        Path path = Paths.get("C:\\Users\\hatik\\Desktop\\Java\\nginx\\pics\\" + modifiedFileName);
+        Files.write(path,bytes);
+        return modifiedFileName;
     }
 }
